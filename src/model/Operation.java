e56af64562f303780bd452baee263b2bc9159540
maria jose <maria.jose.pava@hotmail.com>
2020-09-17 18:12:39 -0500

    Mostrar precios de la obra por cada tienda, nuevo metodo que guarda la cantidad que se necesita de x material, creacion de Javadoc

--- src/model/Operation.java
+++ src/model/Operation.java
@@ -11,6 +11,18 @@ public class Operation {
  public static final double PAINT = 980000;
 
 
+		/**
+
+		Returns the smallest between three numbers
+		<b>pre: </b> <br>
+		<b>post: Calculates what the best price for x item is </b> <br>
+
+		@param a Price in store #1
+		@param b Price in store #2
+		@param c Price in store #3
+		*/
+
+
 	public double bestPrice(double a, double b, double c){
 
 		double lowest = 0;
@@ -22,26 +34,51 @@ public class Operation {
 
 			}
 
-			if (c < a && c < b){
+			else if (c < a && c < b){
 
 				lowest = c;
 			}
 
 
-			if (a < b && a < c){
+			else if (a < b && a < c){
 
 				lowest = a;
 
 			}
 
-			if (a == b && b == c){
+			else if (a == b){
 
 				lowest = a;
+			}
+
+			else if(b == c){
+
+				lowest = b;
+			}
+
+			else if (c == a){
+
+				lowest = c;
 			}
 
 		return lowest;
 
 		}
+
+
+		/**
+
+		 Shows what the best place to purchase an item is depending on its price
+
+		<b>pre:  The best price amongst 3 previously inputted prices has to be calculated and saved to be compared  </b> <br>
+		<b>post: Returns a String with the name of the best place to purchase x item comparing its price at x store with the best price found amongst all stores </b> <br>
+
+		@param bestPrice Best price amongst those introduced by user (at different stores)
+		@param homeCenter Price at Home Center
+		@param ferreteriaBarrio Price at Ferreteria del Barrio
+		@param ferreteriaCentro Price at Ferreteria del Centro
+		*/
+
 
 
 	public String bestPlace(double bestPrice, double homeCenter, double  ferreteriaBarrio, double ferreteriaCentro){
@@ -50,44 +87,59 @@ public class Operation {
 
 				
 
-				if (bestPrice == homeCenter) {
-
-					bestPlace = "Home Center";
-				}
-
-				if (bestPrice== ferreteriaCentro){
-
-					bestPlace = "Ferreteria del Centro";
-				}
-
-				if (bestPrice== ferreteriaBarrio){
-
-					bestPlace = "Ferreteria del Barrio";
-				}
-
-				if (bestPrice==ferreteriaBarrio && bestPrice == ferreteriaCentro){
+				if (bestPrice == ferreteriaBarrio && bestPrice == ferreteriaCentro){
 
 					bestPlace = "Ferreteria del Barrio o Ferreteria del Centro";
 				}
 
-				if (bestPrice==ferreteriaBarrio && bestPrice == homeCenter){
+				else if (bestPrice == ferreteriaBarrio && bestPrice == homeCenter){
 
 					bestPlace = "Ferreteria del Barrio o Home Center";
 				}
 
-				if (bestPrice==ferreteriaCentro && bestPrice == homeCenter){
+				else if (bestPrice == ferreteriaCentro && bestPrice == homeCenter){
 
 					bestPlace = "Ferreteria del Centro o Home Center";
 				}
 
-				if (bestPrice==homeCenter && bestPrice == ferreteriaBarrio && bestPrice == ferreteriaCentro){
+				else if (bestPrice == homeCenter && bestPrice == ferreteriaBarrio && bestPrice == ferreteriaCentro){
 
 					bestPlace = "Home Center,Ferreteria del Centro, Ferreteria del Barrio";
 				}
-					return bestPlace;
+							
+				else if (bestPrice == ferreteriaCentro){
+
+					bestPlace = "Ferreteria del Centro";
+				}
+
+				else if (bestPrice== ferreteriaBarrio){
+
+					bestPlace = "Ferreteria del Barrio";
+				}
+
+				else if (bestPrice == homeCenter) {
+
+					bestPlace = "Home Center";
+				}
+				return bestPlace;
 
 
 		}
+
+
+/**
+
+		
+		Calculates the delivery fee of the products based on the user's location
+
+		<b>pre: Saved an array containing the best prices and saved the location of the user   </b> <br>
+		<b>post: Returns the delivery fee based on location and total cost of items </b> <br>
+
+		@param bestPrice best price amongst those introduced by user. bestPrice != null.
+		@param location user's location. location != "".
+
+		*/
+
 
 
 	public double sumTotal(double[] bestPrice, String location) {
@@ -100,7 +152,6 @@ public class Operation {
 
 			 }
 
-			 System.out.println(sum);
 
 			 if (location.equalsIgnoreCase("n")){
 
@@ -171,6 +222,22 @@ public class Operation {
 
 
 	}
+
+	/**
+
+			Prints what the best price, place and and name of a material for x category
+
+		<b>pre:  Materials array has to be ready and the best prices and places to buy every material have to have been calculated and stored accordingly </b> <br>
+		<b>post: Returns a String with the best place, price and name of every material comparing its index inside a category array with the material's list </b> <br>
+
+		@param numberMaterials total amount of items. numberMaterials > 0.
+		@param stage array of all items belonging to x category.
+		@param materials list of all materials inputted by the user.
+		@param bestPrice best price to buy x item. bestPrice != null.
+		@param bestPlace best place to purchase x item.
+
+		*/
+
 
 
 	public void asignPlace (int numberMaterials, String [] stage, String [] materials, double [] bestPrice, String [] bestPlace){
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -9,6 +9,16 @@ public class Main {
 		public static final double POST_CONSTRUCTION = 2600000;
 		public static final double PAINT = 980000;
 
+		/**
+
+		Returns an integer that represents the amount of materials needed 
+
+		<b>pre: </b> <br>
+		<b>post:  numberMaterials saves the amount of x material requested by the user </b> <br>
+
+		
+		*/
+
 	public static int getNumberMaterials(){
 
 		int numberMaterials;
@@ -20,6 +30,18 @@ public class Main {
 
 		return numberMaterials; 
 	}
+
+
+		/**
+
+		Returns a String with the location of the user 
+
+		<b>pre: </b> <br>
+		<b>post: location saves the location inputted by the user </b> <br>
+
+	
+		*/
+
 
 	public static String getLocation(){
 
@@ -55,6 +77,16 @@ public class Main {
 		return location;
 	}
 
+		/**
+
+		Returns a String with the name of the material inputted by the user 
+		<b>pre: </b> <br>
+		<b>post:  name saves the name of x material inputted by the user</b> <br>
+
+	
+		*/
+
+
 	public static String getName(){
 
 		Scanner input = new Scanner(System.in);
@@ -66,18 +98,88 @@ public class Main {
 
 		}
 
+
+		/**
+
+		Returns an int that represents the category of the material inputted by the user 
+
+		<b>pre: </b> <br>
+		<b>post:   category saves an integer between 1 and 3 that represents a construction stage  (Design stage, Post construction or paint)</b> <br>
+
+	
+		*/
+
 	public static int getCategory(){
 
 		Scanner input = new Scanner(System.in);
+
+		boolean wrongInput = true;
 
 		System.out.println("Ingrese categoria del material: ");
 
 		System.out.println("Obra negra  [1]");
 		System.out.println("Obra blanca [2]");
 		System.out.println("Pintura     [3]");
+
 		int category = input.nextInt();
 
+
+
 		return category;
+
+	}
+
+
+
+		/**
+
+		 Returns the price of x material, inputted by the user, at x store 
+
+		<b>pre:  A String containing the name of x store has to be initialized </b> <br>
+		<b>post: price saves the price of a material at x store </b> <br>
+
+		@param place String containing the name of the store where x element is found
+
+	
+		*/
+
+
+	public static double getPrice(String place){
+
+		double price = 0;
+
+		Scanner input = new Scanner (System.in);
+		System.out.println("Ingrese precio del material en " + place);
+		price = input.nextDouble();
+
+		return price;
+
+
+	}
+
+
+		/**
+
+		Returns an integer that represents the quantity of an item  
+
+		<b>pre:  </b> <br>
+		<b>post: quantity saves the amount of x material needed </b> <br>
+
+	
+		*/
+
+
+
+		public static int getQuantity(){
+
+		int quantity = 0;
+
+		Scanner input = new Scanner (System.in);
+		System.out.println("Ingrese cantidad del material que necesita ");
+		quantity = input.nextInt();
+
+		return quantity;
+
 
 	}
 
@@ -100,8 +202,13 @@ public class Main {
 
 	    double [] bestPrice = new double[numberMaterials];
         String [] bestPlace = new String [numberMaterials];
+
+        String place1 = "Home Center";
+        String place2 = "Ferreteria del Centro";
+        String place3= "Ferreteria del Barrio";
 	    
 		int category = 0;
+		int quantity = 0;
 
 		double total = 0;
 		
@@ -117,14 +224,16 @@ public class Main {
 
 			category = getCategory();
 
-			System.out.println("Ingrese precio del material en Home Center");
-			homeCenter[i]=input.nextDouble();
+			quantity = getQuantity();
 
-			System.out.println("Ingrese precio del material en Ferreteria del Centro");
-			ferreteriaCentro[i]=input.nextDouble();
+			
+			homeCenter[i]= getPrice(place1)*quantity;
 
-			System.out.println("Ingrese precio del material en Ferreteria del Barrio");
-			ferreteriaBarrio[i]=input.nextDouble();
+		
+			ferreteriaCentro[i]=getPrice(place2)*quantity;
+
+			
+			ferreteriaBarrio[i]=getPrice(place3)*quantity;
 
 
 			if (category == 1)
@@ -145,13 +254,12 @@ public class Main {
 
 				bestPlace[x] = op.bestPlace(bestPrice[x], homeCenter[x], ferreteriaBarrio[x], ferreteriaCentro[x]);
 
+
 			}
 
 
-			
-
 			System.out.println("Materiales de OBRA NEGRA:  ");
-			op.asignPlace(numberMaterials, designStage ,materials, bestPrice, bestPlace);
+			op.asignPlace(numberMaterials, designStage, materials, bestPrice, bestPlace);
 
 
 			System.out.println("Materiales de OBRA BLANCA:  ");
@@ -164,8 +272,16 @@ public class Main {
 
 
        total = op.sumTotal(bestPrice, location);
-       System.out.println("El total a pagar es de  " + (total + DESIGN_STAGE + POST_CONSTRUCTION + PAINT));
+       System.out.println("Con los mejores precios el total a pagar es de  " + (total + DESIGN_STAGE + POST_CONSTRUCTION + PAINT));
 
+       total = op.sumTotal(homeCenter, location);
+       System.out.println("Si todo se compra en Home Center el total es de   " + (total + DESIGN_STAGE + POST_CONSTRUCTION + PAINT));
+
+       total = op.sumTotal(ferreteriaBarrio, location);
+       System.out.println("Si todo se compra en Ferreteria del Barrio el total es de   " + (total + DESIGN_STAGE + POST_CONSTRUCTION + PAINT));
+
+       total = op.sumTotal(ferreteriaCentro, location);
+       System.out.println("Si todo se compra en Ferreteria del Centro el total es de   " + (total + DESIGN_STAGE + POST_CONSTRUCTION + PAINT));
 
 
 	}
